ad89073bd3ec6dae2a6dae9bfb2dc52acefa9150
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2012-06-23 16:56:58 +0400

    Optimize capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,16 @@ role :web, application
 set :user, "www-data"
 set :deploy_to, "/var/www/#{application}"
 
+set :composer_path, "#{shared_path}/composer.phar"
+after "deploy:finalize_update", "deploy:update_vendors", "deploy:touch_config"
+
 namespace :deploy do
-    task :finalize_update do
-        run "cd #{release_path} && wget -q http://getcomposer.org/composer.phar && php ./composer.phar install"
+    task :update_vendors do
+        run "mkdir -p #{shared_path}/vendor && ln -s #{shared_path}/vendor #{release_path}/vendor"
+        run "if [ ! -f #{composer_path} ]; then wget -qO #{composer_path} http://getcomposer.org/composer.phar; fi"
+        run "cd #{release_path} && php #{composer_path} install"
+    end
+    task :touch_config do
+        run "touch #{shared_path}/config.php && ln -s #{shared_path}/config.php #{release_path}/app/config.php"
     end
 end
